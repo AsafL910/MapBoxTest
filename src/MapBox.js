a5051ba40d8ed0226842d14650432a35f978b4ff
Asaf <asaflumer@gmail.com>
2022-06-06 17:27:51 +0300

    Initial Map Created

---
+++ src/MapBox.js
@@ -0,0 +1,38 @@
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { useState, useEffect, useRef } from "react";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYmFydWNoZ2luemJ1cmciLCJhIjoiY2trMTFja3FoMG4wOTJ2cnJnZzU5bDc2NSJ9.K3acyuJruEbTLgDD5KrC2A";
+
+const MapBox = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" style={{height: "1440px"}} />
+    </div>
+    );
+};
+
+export default MapBox;
+
+// const accessToken = "pk.eyJ1IjoiYmFydWNoZ2luemJ1cmciLCJhIjoiY2trMTFja3FoMG4wOTJ2cnJnZzU5bDc2NSJ9.K3acyuJruEbTLgDD5KrC2A"
+// const tilesetId = "mapbox.mapbox-streets-v8"
+// const zoom = 1
+// const x = 0
+// const  y = 0
+// const api = `https://api.mapbox.com/v4/${tilesetId}/${zoom}/${x}/${y}.mvt?access_token=${accessToken}`
