a81a86323d973e148f049dd6cd7a9fa95620af20
Asaf <asaflumer@gmail.com>
2023-01-03 12:59:49 +0200

    Reverted back to fetch
    Added option for more planes

--- src/components/SideMenu/FetchOtherPlanes.js
+++ src/components/SideMenu/FetchOtherPlanes.js
@@ -2,12 +2,7 @@ import { Layer, Source, useMap } from "react-map-gl";
 import { useEffect, useRef, useState } from "react";
 import ConnectingAirportsIcon from "@mui/icons-material/ConnectingAirports";
 import SideMenuBtn from "./SideMenuBtn";
-import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { Snackbar } from "@mui/material";
-
-
-
-
 
 const FetchOtherPlanes = ({ planeCount, isFetchingAllPlanes }) => {
   const isFetchingAllPlanesRef = useRef();
@@ -21,10 +16,9 @@ const FetchOtherPlanes = ({ planeCount, isFetchingAllPlanes }) => {
   });
   const otherPlanesDataRef = useRef();
   otherPlanesDataRef.current = otherPlanesData;
-
   const [isPlaneClicked, setIsPlaneClicked] = useState(false);
   const [lastPlaneClicked, setLastPlaneClicked] = useState(null);
-  const websocketRef = useRef(null)
+  const [isFetching, setIsFetching] = useState(false)
 
   const otherPlanesLayer = {
     id: "otherPlanes",
@@ -44,12 +38,10 @@ const FetchOtherPlanes = ({ planeCount, isFetchingAllPlanes }) => {
       if (error) throw error;
       currMap.addImage("selfDataIcon", image);
 
-      currMap.on("click", "otherPlanes", (e) =>
-      {
-        setLastPlaneClicked(e.features[0].properties.callSign);
-        setIsPlaneClicked(true);
-      }
-      );
+      currMap.on("click", "otherPlanes", (e) =>{
+      setLastPlaneClicked(e.features[0].properties.callSign);
+      setIsPlaneClicked(true);
+    });
       currMap.on(
         "mouseenter",
         "otherPlanes",
@@ -63,61 +55,43 @@ const FetchOtherPlanes = ({ planeCount, isFetchingAllPlanes }) => {
     });
   }, []);
 
-  useEffect(()=>{
-    const initConnection = () => {
-      const planesDataClient = new W3CWebSocket(
-        "ws://localhost:2000/other-planes"
-      );
-      planesDataClient.onopen = () => {
-        console.log("Client Connected to position provider!");
-      };
-      planesDataClient.onmessage = (message) => {
-        const data = JSON.parse(message.data);
-        console.log("recieved update")
-        const parsedData = data.map((plane) => {
-          return {
-            type: "Feature",
-            properties: {
-              callSign: plane.CallSign,
-              trueTrack: plane.TrueTrack,
-            },
-            geometry: {
-              type: "Point",
-              coordinates: [plane.Position.Longitude, plane.Position.Latitude],
-            },
-          };
-        });
-  
-        setOtherPlanesData({
-          type: "FeatureCollection",
-          features: [...parsedData],
-        });
-    }
-    planesDataClient.onerror = (error) => {
-      console.log(error)
+  const fetchForOtherPlanes = async () => {
+      while(true) {
+      const data = await (
+        await fetch(
+          `http://localhost:5000/multi-position/${
+            isFetchingAllPlanesRef.current  ? "" : planeCountRef.current
+          }`
+        )
+      ).json();
+      const parsedData = data.map((plane) => {
+        return {
+          type: "Feature",
+          properties: {
+            callSign: plane.callSign,
+            trueTrack: plane.trueTrack,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [plane.position.longitude, plane.position.latitude],
+          },
+        };
+      });
+
+      setOtherPlanesData({
+        type: "FeatureCollection",
+        features: [...parsedData],
+      });
     }
 
-    planesDataClient.onclose = (message) => {
-      console.log("Connection ended")
-      initConnection()
-    }
-
-    websocketRef.current = planesDataClient;
-    }
-
-    initConnection()
-
-    return () => {
-      websocketRef.current.close();
-    }
-  },[])
+  };
 
   return (
     <>
-    <Snackbar open={isPlaneClicked} onClose={()=>{setIsPlaneClicked(false);}} message={lastPlaneClicked}/>
+      <Snackbar open={isPlaneClicked} onClose={()=>{setIsPlaneClicked(false);}} message={lastPlaneClicked} anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}/>
       <SideMenuBtn
         onClick={() => {
-          websocketRef.current?.send(JSON.stringify(planeCountRef.current))
+          fetchForOtherPlanes();
         }}
         Icon={ConnectingAirportsIcon}
         className={planeCount <= 0 && !isFetchingAllPlanes && " disabled"}
